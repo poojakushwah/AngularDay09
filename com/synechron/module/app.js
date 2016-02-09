21ecaea690f7d2d3624261e2b6ba8f51e659218f
unknown <SachinYe@.synechron.com>
2016-02-09 14:32:06 +0530

    All updaetd

--- com/synechron/module/app.js
+++ com/synechron/module/app.js
@@ -1,16 +1,13 @@
-/**
- * @author SachinYe
- */
-{function(){
-	angular.module("app",["ngRouter"])
-	.config(funciton($routeProvider){
-		.when("/",{
-			controller:"herocontroller",
-			templateUrl:"com/synechron/views/heros.html"
-		})
-		.when("/movies",{
-			controller:"moviescontroller",
-			templateUrl:"com/synechron/views/movies.html"
-		})
-	})
-}()}
+( function() {
+		angular.module("app", ["ngRoute"]);
+		angular.module("app").config(function($routeProvider) {
+			$routeProvider
+			.when("/", {
+				controller : "herocontroller",
+				templateUrl : "com/synechron/views/heros.html"
+			}).when("/movies/:hid", {
+				controller : "moviescontroller",
+				templateUrl : "com/synechron/views/movies.html"
+			});
+		});
+	}());
